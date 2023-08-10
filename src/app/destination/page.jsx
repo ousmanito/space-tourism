e111742c4a066c629522f7c0bc987d570a69802d
Ousmane <ousmane.209@gmail.com>
2023-08-10 08:56:10 +0200

    feat(destination): add background image styling

---
+++ src/app/destination/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./destination.module.css";
+
+export default function Page() {
+	return (
+		<>
+			<span className={styles.bg}></span>
+		</>
+	);
+}
