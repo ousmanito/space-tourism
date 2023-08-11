28a48cbf09c2bef30515ffa9b3d92a01d4bd1391
Ousmane <ousmane.209@gmail.com>
2023-08-11 13:28:34 +0200

    style: update destination module styles

--- src/app/destination/page.jsx
+++ src/app/destination/page.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./destination.module.css";
 import Image from "next/image";
-import moon from "/public/assets/destination/image-moon.webp";
 import { bellefair } from "../fonts";
+import planets from "public/assets/data.json";
 
 export default function Page() {
+  const [planet, setPlanet] = useState("Moon");
+  const planetObject = planets.destinations.find((p) => {
+    return p.name == planet;
+  });
+  const imageUrl = `/assets/destination/image-${planet.toLowerCase()}.webp`;
+  console.log(imageUrl);
+  const desc = planetObject.description;
+  const averageDist = planetObject.distance;
+  const travelTime = planetObject.travel;
   return (
     <>
       <span className={styles.bg}></span>
@@ -15,36 +25,44 @@ export default function Page() {
         </div>
         <div className={styles.main}>
           <div className={styles.image}>
-            <Image src={moon} alt="moon" width={"100%"} />
+            <Image
+              src={imageUrl}
+              alt="planet-image"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "100%", height: "auto" }}
+            />
           </div>
           <div className={styles.content}>
             <div className={styles.nav}>
               <ul>
-                <li>Moon</li>
-                <li>Mars</li>
-                <li>Europa</li>
-                <li>Titan</li>
+                {planets.destinations.map((obj, index) => {
+                  console.log(planet, obj.name);
+                  return (
+                    <li
+                      key={index}
+                      className={planet == obj.name ? styles.active : ""}
+                      onClick={(e) => setPlanet(e.target.innerHTML)}
+                    >
+                      {obj.name}
+                    </li>
+                  );
+                })}
               </ul>
             </div>
             <div className={styles.text}>
-              <h1>Moon</h1>
-              <p>
-                Lorem ipsum dolor sit amet, officia excepteur ex fugiat
-                reprehenderit enim labore culpa sint ad nisi Lorem pariatur
-                mollit ex esse exercitation amet. Nisi anim cupidatat excepteur
-                officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip
-                amet voluptate voluptate dolor minim nulla est proident. Nostrud
-                officia pariatur ut officia.
-              </p>
+              <h1>{planet}</h1>
+              <p>{desc}</p>
               <div className={styles.line}>line</div>
               <div className={styles.footer}>
                 <div className={styles.distance}>
                   <h3>AVG. DISTANCE</h3>
-                  <h2>100,000 km</h2>
+                  <h2>{averageDist}</h2>
                 </div>
                 <div className={styles.time}>
                   <h3>EST. TRAVEL TIME</h3>
-                  <h2>25 days</h2>
+                  <h2>{travelTime}</h2>
                 </div>
               </div>
             </div>
