6edb83b109107f84ac523afa3f2aa0eeddba283b
Ousmane <ousmane.209@gmail.com>
2023-09-10 22:31:50 +0200

    styles: make crew responsive w/ new layout that works

--- src/app/crew/page.tsx
+++ src/app/crew/page.tsx
@@ -28,7 +28,33 @@ type Crew = {
   images: Images;
   role: string;
   bio: string;
+  missions: string[];
+  birthdate: string;
+  durationMissions: string;
 };
+
+const getMissionDuration = (duration: string): string => {
+  const [days, hours] = duration.split(' ');
+  let result = '';
+  if (parseInt(days)) {
+    result += `${parseInt(days)} day${parseInt(days) > 1 ? 's' : ''}, `;
+  }
+  result += `${parseInt(hours)} hour${parseInt(hours) > 1 ? 's' : ''}`;
+
+  return result;
+}
+
+const getAge = (birthdate: string): string => {
+  const [day, month, year] = birthdate.split('/');
+  const birthDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return ` (${age} year${age > 1 ? 's' : ''})`;
+}
 
 export default function Page() {
   const next = () => {
@@ -54,6 +80,9 @@ export default function Page() {
   const role = crewObject.role;
   const bio = crewObject.bio;
   const name = crewObject.name;
+  const birthdate = crewObject.birthdate;
+  const missions = crewObject.missions;
+  const durationMissions = getMissionDuration(crewObject.durationMissions)
   const imageUrl = crewObject.images.webp;
   return (
     <>
@@ -95,15 +124,34 @@ export default function Page() {
               </span>
             </div>
           </div>
-          <div className={styles.right}>
-            <Image
-              src={imageUrl}
-              alt="staff-body"
-              sizes="100vh"
-              style={{ height: "100%", width: "fit-content" }}
-              width={0}
-              height={0}
-            />
+          <div className={styles.card}>
+            <div className={styles.image}>
+
+              <img
+                src={imageUrl}
+                alt="staff-body"
+              />
+
+            </div>
+            <div className={styles.details}>
+
+              <div className={styles.birthdate}>
+                <h2>Born</h2>
+                <h3>{birthdate.concat(' ', getAge(birthdate))}</h3>
+              </div>
+              <ul className={styles.missions}>
+                <h2>Missions</h2>
+                {missions.map((m, id) => {
+                  return (
+                    <li className="list-style-def" key={id}>{m}</li>
+                  )
+                })}
+              </ul>
+              <div className={styles.duration}>
+                <h2>Time in space</h2>
+                <h3>{durationMissions}</h3>
+              </div>
+            </div>
           </div>
         </div>
       </div>
