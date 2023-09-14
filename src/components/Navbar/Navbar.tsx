3a47c2ec8bfcd6db6f1944f432cd26ab0c9f1045
Ousmane <ousmane.209@gmail.com>
2023-09-14 17:06:59 +0200

    style: update Navbar toggle icon color

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -19,33 +19,28 @@ type Pages = {
 };
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(true);
-  const [menu, setMenu] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1400);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   useEffect(() => {
-    if (typeof window != undefined) {
-      if (window.innerWidth > 1400) {
-        setIsOpen(false);
-      } else {
-        setIsOpen(true);
+    const handleResize = () => {
+      const desktop = window.innerWidth >= 1400;
+      setIsDesktop(desktop);
+      if (desktop) {
+        setIsMenuOpen(false); 
       }
-      window.addEventListener("resize", () => {
-        let width = window.innerWidth;
-        if (width > 1400) {
-          setIsOpen(false);
-        } else {
-          setIsOpen(true);
-        }
-      });
-    }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); 
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
+
   const toggleMenu = () => {
-    setMenu(!menu);
-    let sidebar: HTMLElement | null = document.querySelector(
-      `.${styles["sidebar-container"]}`,
-    );
-    if (sidebar) {
-      sidebar.classList.toggle(styles.open);
-    }
+    setIsMenuOpen(!isMenuOpen);
   };
   let pathname = usePathname();
   const links: Pages = {
@@ -63,12 +58,17 @@ const Navbar = () => {
             <Image src={Logo} alt="logo" className={styles.logo} />
           </a>
 
-          {isOpen && (
+          {!isDesktop && (
             <span className={styles["toggle-btn"]} onClick={() => toggleMenu()}>
-              <Image src={menu ? close : bars} width={30} alt="toggleIcon" className={styles.toggler} />
+              <Image
+                src={isMenuOpen? close : bars}
+                width={30}
+                alt="toggleIcon"
+                className={styles.toggler}
+              />
             </span>
           )}
-          {!isOpen && (
+          {isDesktop && (
             <div className={styles["navlist-container"]}>
               <ul>
                 {Object.keys(links).map((link: string, index: number) => {
@@ -102,36 +102,38 @@ const Navbar = () => {
           )}
         </div>
       </nav>
-      <div className={styles["sidebar-container"]}>
-        <ul className={styles["sidebar"]}>
-          {Object.keys(links).map((link: string, index: number) => {
-            console.log(link, index);
-            if (pathname === "/") pathname = "/home";
-            const isActive = pathname.match("/".concat(link));
-            return (
-              <li
-                onClick={() => toggleMenu()}
-                className={[styles["nav-text"], barlow.className].join(" ")}
-                key={index}
-              >
-                <Link href={link}>
-                  <span className={styles["nav-item__index"]}>
-                    {String(index).padStart(2, "0")}
-                  </span>
-                  <span
-                    className={[
-                      styles["nav-item__name"],
-                      isActive ? styles.active : null,
-                    ].join(" ")}
-                  >
-                    {links[link]}
-                  </span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      {!isDesktop && isMenuOpen && (
+        <div className={styles["sidebar-container"]}>
+          <ul className={styles["sidebar"]}>
+            {Object.keys(links).map((link: string, index: number) => {
+              console.log(link, index);
+              if (pathname === "/") pathname = "/home";
+              const isActive = pathname.match("/".concat(link));
+              return (
+                <li
+                  onClick={() => toggleMenu()}
+                  className={[styles["nav-text"], barlow.className].join(" ")}
+                  key={index}
+                >
+                  <Link href={link}>
+                    <span className={styles["nav-item__index"]}>
+                      {String(index).padStart(2, "0")}
+                    </span>
+                    <span
+                      className={[
+                        styles["nav-item__name"],
+                        isActive ? styles.active : null,
+                      ].join(" ")}
+                    >
+                      {links[link]}
+                    </span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
