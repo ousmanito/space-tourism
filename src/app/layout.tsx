47076c9c4054aa51455cb3033f0d9abfa03d541a
Ousmane <ousmane.209@gmail.com>
2023-09-15 22:09:47 +0200

    style: Update global CSS and SCSS files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,27 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import Navbar from '@/components/Navbar/Navbar'
-import { barlow } from './fonts'
-
+import "./globals.css";
+import type { Metadata } from "next";
+import Navbar from "@/components/Navbar/Navbar";
+import { barlow, bellefair } from "./fonts";
 
 export const metadata: Metadata = {
-  title: 'Space Challenge V2',
-  description: 'FrontEndChallenge',
-}
+  title: "Space Challenge V2",
+  description: "FrontEndChallenge",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={barlow.className}>
-        <Navbar/>
+      <body className={[bellefair.variable, barlow.className].join(' ')}>
+        <Navbar />
         {children}
-        </body>
+        <p className='footer'>
+          © 2023 Ousmane Bathily. All rights reserved
+        </p>
+      </body>
     </html>
-  )
+  );
 }
