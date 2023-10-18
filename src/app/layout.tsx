557095e4c81fbf307a712069821dbeace892bcb8
Ousmane <ousmane.209@gmail.com>
2023-10-18 02:37:04 +0200

    docs: Update metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import Navbar from "@/components/Navbar/Navbar";
 import { barlow, bellefair } from "./fonts";
 
 export const metadata: Metadata = {
-  title: "Space Challenge V2",
-  description: "FrontEndChallenge",
+  title: "Space Tourism",
+  description: "Are you interested in spatial exploration ? We will provide you some basic informations about that topic, buckle up !",
 };
 
 export default function RootLayout({
